b2351358ab23e5f88fac91a7b111162c5889ecfb
shwetal14 <95999701+shwetal14@users.noreply.github.com>
2021-12-30 00:01:12 +0530

    Add files via upload

---
+++ sample 1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+def sum(numbers):
+    total = 0
+    for x in numbers:
+        total += x
+    return total
+print(sum((8, 2, 3, 0, 7)))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
